f72a8b308789b2137cdfaec5b573b61368ed181d
Zen <zainulfirdaus123@gmail.com>
2026-07-13 13:21:27 +0700

    Initial Commit

---
+++ OlahragaApp/OlahragaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlahragaAppApp.swift
+//  OlahragaApp
+//
+//  Created by Moh Zainul Firdaus on 13/07/26.
+//
+
+import SwiftUI
+
+@main
+struct OlahragaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
